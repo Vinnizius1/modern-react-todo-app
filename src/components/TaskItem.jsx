18690c8163a7160cca76a5e59e794e03c07c7667
vinicius <vmm_7@hotmail.com>
2022-09-13 21:39:18 -0300

    Lógica do 'checked' funcionando!

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+// styles
+import styles from "./TaskItem.module.css";
 
-function TaskItem({task}) {
+// Library imports
+import { CheckIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
+
+function TaskItem({ task }) {
+  const [isChecked, setIsChecked] = useState(task.checked);
+
+  const handleCheckboxChange = (e) => {
+    setIsChecked(!isChecked);
+  };
+
   return (
-    <div>
-        {task.name}
-    </div>
-  )
+    <li className={styles.task}>
+      <div className={styles["task-group"]}>
+        <input
+          type="checkbox"
+          checked={isChecked}
+          className={styles.checkbox}
+          onChange={handleCheckboxChange}
+          name={task.name}
+          id={task.id}
+        />
+        <label htmlFor={task.id} className={styles.label}>
+          {task.name}
+          <p className={styles.checkmark}>
+            <CheckIcon strokeWidth={2} width={24} height={24} />
+          </p>
+        </label>
+      </div>
+    </li>
+  );
 }
 
-export default TaskItem
+export default TaskItem;
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -8,10 +8,13 @@ import styles from "./TaskList.module.css";
 
 export default function TaskList({ tasks }) {
   return (
-    <ul className={StyleSheet.tasks}>
+    <ul className={styles.tasks}>
       {tasks.sort((a, b) => b.id - a.id).map((task) => (
-        <TaskItem key={task.id} task={task} />
-      ))}
+        <TaskItem 
+        key={task.id} 
+        task={task} />
+      ))
+      }
     </ul>
   );
 }
